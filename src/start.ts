c3b0aaf3f9c7e6bc60cf91f8dfb83d67bee82c20
Emma RileyIO <riley@tdm.io>
2020-06-17 22:17:59 -0300

    Printing HTTPS or HTTP on startup msg

--- src/start.ts
+++ src/start.ts
@@ -3,5 +3,5 @@ import { Server } from '.'
 const server = new Server()
 
 server.server.listen(8235, () => {
-  console.log(`kiera-web listening on: 8235`)
+  console.log(`kiera-web listening on: 8235`, server.isHTTPSSet ? 'https' : 'http')
 })
